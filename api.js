59ac5ae88f676f97f5726ce8c2d8bff0648663c4
Imperator070 <k.bykov.06@mail.ru>
2026-06-04 10:18:34 +0300

    changes

--- api.js
+++ api.js
@@ -1,61 +1,61 @@
 class ProductApiService {
-    constructor(baseUrl = 'http://localhost:3000') {
-        this.baseUrl = baseUrl;
-    }
+  constructor(baseUrl = 'http://localhost:3000') {
+    this.baseUrl = baseUrl;
+  }
 
-    getAll(success, error) {
-        const xhr = new XMLHttpRequest();
-        xhr.open('GET', `${this.baseUrl}/stocks`, true);
-        xhr.setRequestHeader('Content-Type', 'application/json');
-        xhr.onload = () => xhr.status >= 200 && xhr.status < 300
-            ? success(JSON.parse(xhr.responseText))
-            : error(xhr.status, xhr.responseText);
-        xhr.onerror = () => error(xhr.status, 'Network error');
-        xhr.send();
-    }
+  getAll(success, error) {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', `${this.baseUrl}/stocks`, true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.onload = () => xhr.status >= 200 && xhr.status < 300
+      ? success(JSON.parse(xhr.responseText))
+      : error(xhr.status, xhr.responseText);
+    xhr.onerror = () => error(xhr.status, 'Network error');
+    xhr.send();
+  }
 
-    getById(id, success, error) {
-        const xhr = new XMLHttpRequest();
-        xhr.open('GET', `${this.baseUrl}/stocks/${id}`, true);
-        xhr.setRequestHeader('Content-Type', 'application/json');
-        xhr.onload = () => xhr.status >= 200 && xhr.status < 300
-            ? success(JSON.parse(xhr.responseText))
-            : error(xhr.status, xhr.responseText);
-        xhr.onerror = () => error(xhr.status, 'Network error');
-        xhr.send();
-    }
+  getById(id, success, error) {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', `${this.baseUrl}/stocks/${id}`, true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.onload = () => xhr.status >= 200 && xhr.status < 300
+      ? success(JSON.parse(xhr.responseText))
+      : error(xhr.status, xhr.responseText);
+    xhr.onerror = () => error(xhr.status, 'Network error');
+    xhr.send();
+  }
 
-    create(data, success, error) {
-        const xhr = new XMLHttpRequest();
-        xhr.open('POST', `${this.baseUrl}/stocks`, true);
-        xhr.setRequestHeader('Content-Type', 'application/json');
-        xhr.onload = () => xhr.status >= 200 && xhr.status < 300
-            ? success(JSON.parse(xhr.responseText))
-            : error(xhr.status, xhr.responseText);
-        xhr.onerror = () => error(xhr.status, 'Network error');
-        xhr.send(JSON.stringify(data));
-    }
+  create(data, success, error) {
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', `${this.baseUrl}/stocks`, true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.onload = () => xhr.status >= 200 && xhr.status < 300
+      ? success(JSON.parse(xhr.responseText))
+      : error(xhr.status, xhr.responseText);
+    xhr.onerror = () => error(xhr.status, 'Network error');
+    xhr.send(JSON.stringify(data));
+  }
 
-    update(id, data, success, error) {
-        const xhr = new XMLHttpRequest();
-        xhr.open('PATCH', `${this.baseUrl}/stocks/${id}`, true);
-        xhr.setRequestHeader('Content-Type', 'application/json');
-        xhr.onload = () => xhr.status >= 200 && xhr.status < 300
-            ? success(JSON.parse(xhr.responseText))
-            : error(xhr.status, xhr.responseText);
-        xhr.onerror = () => error(xhr.status, 'Network error');
-        xhr.send(JSON.stringify(data));
-    }
+  update(id, data, success, error) {
+    const xhr = new XMLHttpRequest();
+    xhr.open('PATCH', `${this.baseUrl}/stocks/${id}`, true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.onload = () => xhr.status >= 200 && xhr.status < 300
+      ? success(JSON.parse(xhr.responseText))
+      : error(xhr.status, xhr.responseText);
+    xhr.onerror = () => error(xhr.status, 'Network error');
+    xhr.send(JSON.stringify(data));
+  }
 
-    delete(id, success, error) {
-        const xhr = new XMLHttpRequest();
-        xhr.open('DELETE', `${this.baseUrl}/stocks/${id}`, true);
-        xhr.onload = () => xhr.status >= 200 && xhr.status < 300
-            ? success()
-            : error(xhr.status, xhr.responseText);
-        xhr.onerror = () => error(xhr.status, 'Network error');
-        xhr.send();
-    }
+  delete(id, success, error) {
+    const xhr = new XMLHttpRequest();
+    xhr.open('DELETE', `${this.baseUrl}/stocks/${id}`, true);
+    xhr.onload = () => xhr.status >= 200 && xhr.status < 300
+      ? success()
+      : error(xhr.status, xhr.responseText);
+    xhr.onerror = () => error(xhr.status, 'Network error');
+    xhr.send();
+  }
 }
 
 window.api = new ProductApiService();
